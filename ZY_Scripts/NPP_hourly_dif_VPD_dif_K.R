30d7e97dfca68f1f8ab3edd6531385d9f043cf21
Zhuoran Yu <zhuoranyu26@gmail.com>
2023-10-04 14:48:24 -0400

    more update

--- ZY_Scripts/NPP_hourly_dif_VPD_dif_K.R
+++ ZY_Scripts/NPP_hourly_dif_VPD_dif_K.R
@@ -11,7 +11,7 @@ frac.liana.al = 0.1
 
 tree.dbh = mean.hori.data.tree.dbh
 liana.length = (tree.b1Ht * tree.dbh^tree.b2Ht)
-K=5805.55556
+#K=5805.55556
 
 each_day = c(rep(0,times=6),rep(1, times=12),rep(0,times=6))
 year_day_light = c(rep(each_day, times=366))
@@ -29,30 +29,120 @@ vpd_year <- cbind(vpd_year,year_vpd_org,year_day_light)
 liana_npp_org_VPD = rep(0,times=length(year_day_light))
 liana_npp_var_VPD = rep(0,times=length(year_day_light))
 
+sorted.K <- sort(lianas_K$K, decreasing = FALSE)
+
+sorted.K.mPa <- sorted.K/1000
+
+liana_npp_org_VPD_low_k = rep(0,8784)
+liana_npp_var_VPD_low_k = rep(0,8784)
+liana_npp_org_VPD_mid_k = rep(0,8784)
+liana_npp_var_VPD_mid_k = rep(0,8784)
+liana_npp_org_VPD_high_k = rep(0,8784)
+liana_npp_var_VPD_high_k = rep(0,8784)
+#lowest K
 for (i in 1:length(year_day_light)){
   psis= vpd_year$SWP
   day_light = vpd_year$year_day_light
-  liana_npp_org_VPD[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = K, tot.al = tot.al, nday=nday,month=month,
+  liana_npp_org_VPD_low_k[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = sorted.K.mPa[1], tot.al = tot.al, nday=nday,month=month,
                                               frac.liana.al = frac.liana.al, liana.length = liana.length,
                                          day_light=day_light,psis = psis,D_vec=vpd_year$year_vpd_org)
   
-  liana_npp_var_VPD[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = K, tot.al = tot.al, nday=nday,month=month,
+  liana_npp_var_VPD_low_k[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = sorted.K.mPa[1], tot.al = tot.al, nday=nday,month=month,
                                             frac.liana.al = frac.liana.al, liana.length = liana.length,
                                             day_light=day_light,psis = psis,D_vec=vpd_year$vpd_pa)
+  liana_npp_org_VPD_mid_k[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = sorted.K.mPa[25], tot.al = tot.al, nday=nday,month=month,
+                                                  frac.liana.al = frac.liana.al, liana.length = liana.length,
+                                                  day_light=day_light,psis = psis,D_vec=vpd_year$year_vpd_org)
+  
+  liana_npp_var_VPD_mid_k[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = sorted.K.mPa[25], tot.al = tot.al, nday=nday,month=month,
+                                                  frac.liana.al = frac.liana.al, liana.length = liana.length,
+                                                  day_light=day_light,psis = psis,D_vec=vpd_year$vpd_pa)
+  
+  liana_npp_org_VPD_high_k[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = sorted.K.mPa[51], tot.al = tot.al, nday=nday,month=month,
+                                                  frac.liana.al = frac.liana.al, liana.length = liana.length,
+                                                  day_light=day_light,psis = psis,D_vec=vpd_year$year_vpd_org)
+  
+  liana_npp_var_VPD_high_k[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = sorted.K.mPa[51], tot.al = tot.al, nday=nday,month=month,
+                                                  frac.liana.al = frac.liana.al, liana.length = liana.length,
+                                                  day_light=day_light,psis = psis,D_vec=vpd_year$vpd_pa)
   
 }
 
-liana.data <- cbind(vpd_year,liana_npp_org_VPD,liana_npp_var_VPD)
+liana.data <- cbind(vpd_year,liana_npp_org_VPD_low_k,liana_npp_var_VPD_low_k,
+                    liana_npp_org_VPD_mid_k,liana_npp_var_VPD_mid_k,
+                    liana_npp_org_VPD_high_k,liana_npp_var_VPD_high_k)
 
 
 liana.monthly <- liana.data %>%
   group_by(month) %>%
   summarise( 
-    liana.npp.month.org = sum(liana_npp_org_VPD),
-    liana.npp.month.var = sum(liana_npp_var_VPD)
+    liana.npp.month.org.low = sum(liana_npp_org_VPD_low_k),
+    liana.npp.month.var.low = sum(liana_npp_var_VPD_low_k),
+    liana.npp.month.org.mid = sum(liana_npp_org_VPD_mid_k),
+    liana.npp.month.var.mid = sum(liana_npp_var_VPD_mid_k),
+    liana.npp.month.org.high = sum(liana_npp_org_VPD_high_k),
+    liana.npp.month.var.high = sum(liana_npp_var_VPD_high_k)
     ) 
- summary(liana.monthly$liana.npp.month.var)
-plot(liana.monthly$month,liana.monthly$liana.npp.month.org,ylim =c(-0.5,3))
-points(liana.monthly$month,liana.monthly$liana.npp.month.var,col="green")
-sum(liana_npp_org_VPD)
-sum(liana_npp_var_VPD)
+
+sum(liana.monthly$liana.npp.month.org.low)
+sum(liana.monthly$liana.npp.month.var.low)
+sum(liana.monthly$liana.npp.month.org.mid)
+sum(liana.monthly$liana.npp.month.var.mid)
+sum(liana.monthly$liana.npp.month.org.high)
+sum(liana.monthly$liana.npp.month.var.high)
+
+summary(liana.monthly$liana.npp.month.org.low)
+summary(liana.monthly$liana.npp.month.var.low)
+plot(liana.monthly$month,liana.monthly$liana.npp.month.org.low,ylim =c(-1,3),main = "K=28.88889",)
+points(liana.monthly$month,liana.monthly$liana.npp.month.var.low,col="green")
+legend(0, 2, legend=c("original", "variation"),
+       col=c("black", "green"), lty=1:2, cex=0.8)
+
+summary(liana.monthly$liana.npp.month.org.mid)
+summary(liana.monthly$liana.npp.month.var.mid)
+plot(liana.monthly$month,liana.monthly$liana.npp.month.org.mid,ylim =c(-1,3),main = "K=512.8449",)
+points(liana.monthly$month,liana.monthly$liana.npp.month.var.mid,col="green")
+legend(0, 2, legend=c("original", "variation"),
+       col=c("black", "green"), lty=1:2, cex=0.8)
+
+
+summary(liana.monthly$liana.npp.month.org.high)
+summary(liana.monthly$liana.npp.month.var.high)
+plot(liana.monthly$month,liana.monthly$liana.npp.month.org.high,ylim =c(-1,3),main = "K=5805.55556",)
+points(liana.monthly$month,liana.monthly$liana.npp.month.var.high,col="green")
+legend(0, 2, legend=c("original", "variation"),
+       col=c("black", "green"), lty=1:2, cex=0.8)
+
+
+
+df[[text]] <- ifelse(df$season == i, 1, 0)
+rm(dt)
+dt <- matrix(0, 8784, 2) 
+dt <-data.frame(dt)
+
+for (K in sorted.K.mPa[1:2]){
+  for (i in 1:length(year_day_light)){
+  psis= vpd_year$SWP
+  day_light = vpd_year$year_day_light
+  liana_npp_org_VPD[i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = K, tot.al = tot.al, nday=nday,month=month,
+                                            frac.liana.al = frac.liana.al, liana.length = liana.length,
+                                            day_light=day_light,psis = psis,D_vec=vpd_year$year_vpd_org)}
+  
+  dt <- matrix(0, 8784, 2) 
+  dt <-data.frame(dt)
+  dt[,K] <- liana_npp_org_VPD[i]
+  colnames(dt)<-paste0("K_",K))}}
+  
+  dt[,K]=output
+  #liana.data$liana_npp_org_VPD[text][i] = liana.NPP.mxh.hour(dbh = dbh, liana.k = K, tot.al = tot.al, nday=nday,month=month,
+                                           # frac.liana.al = frac.liana.al, liana.length = liana.length,
+                                           # day_light=day_light,psis = psis,D_vec=vpd_year$vpd_pa)
+  
+}
+
+
+#####codes that did not work
+#text <- paste("liana_npp_org_VPD_K", K)
+#dt[,paste("K_value", K, sep="")] = rep(0, each=8784)}
+#df$liana_npp_var_VPD[[text]] = rep(0,times=length(year_day_light))
+
